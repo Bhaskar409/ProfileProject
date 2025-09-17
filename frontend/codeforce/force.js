37302f7c590ef5ac24b4d85284346582a13e1b03
Bhaskar Kumar <bhaskar77277@gmail.com>
2025-09-17 16:52:25 +0530

    working with the codeforce api

--- frontend/codeforce/force.js
+++ frontend/codeforce/force.js
@@ -14,18 +14,56 @@
 
 
 
+// function forceapicall()
+// {
+//     fetch('https://codeforces.com/api/contest.list')
+//     .then(function(response)
+//     {
+//         if(response.ok)
+//             console.log('Server working fine');
+//         else
+//             console.log('server responds with error status');
+//     })
+//     .catch(function(err)
+//     {
+//         console.log('server errr',err);
+//     })
+// }
+
+
 function forceapicall()
 {
-    fetch('https://codeforces.com/api/contest.list')
+    const idforce = document.getElementById("forceid").value;
+    const resultgain = document.getElementById("resultcodeforce")
+
+    if(idforce === "")
+    {
+        resultgain.innerHTML = "enter your id";
+        return ;
+    }
+
+    fetch(`https://codeforces.com/api/user.info?handles=${idforce}`)
     .then(function(response)
     {
-        if(response.ok)
-            console.log('Server working fine');
-        else
-            console.log('server responds with error status');
+        return response.json();
+    })
+    .then(function(data)
+    {
+        if (data.status !== "OK" || !data.result.length) {
+                resultgain.textContent = "User not found.";
+                return;
+        }
+        const user = data.result[0];
+
+        resultgain.innerHTML = `
+            <p>Handle: ${user.handle}</p>
+            <p>Rank: ${user.rank}</p>
+        `;
     })
     .catch(function(err)
     {
-        console.log('server errr',err);
-    })
+        console.log(err);
+    });
+
+
 }
